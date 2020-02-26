18f44c8f28d0ca783c2fe63b28fe37d9ad6c7578
Christopher Fuentes <fuenteschris75@gmail.com>
2020-02-26 16:40:39 -0600

    Finishing interes compuesto

--- dist/js/pages/dashboard3.js
+++ dist/js/pages/dashboard3.js
@@ -19,11 +19,6 @@ $(function () {
           backgroundColor: '#007bff',
           borderColor    : '#007bff',
           data           : [1000, 2000, 3000, 2500, 2700, 2500, 3000]
-        },
-        {
-          backgroundColor: '#ced4da',
-          borderColor    : '#ced4da',
-          data           : [700, 1700, 2700, 2000, 1800, 1500, 2000]
         }
       ]
     },
@@ -138,3 +133,4 @@ $(function () {
     }
   })
 })
+t
